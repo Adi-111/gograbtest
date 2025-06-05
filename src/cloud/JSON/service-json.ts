8ae4e17c04473a13e29a2940e170a37e65572236
Adi-111 <Chandradityasingh11@gmail.com>
2025-06-05 18:23:16 +0530

    Signed-off-by: Adi-111 <Chandradityasingh11@gmail.com>

--- src/cloud/JSON/service-json.ts
+++ src/cloud/JSON/service-json.ts
@@ -1,15 +1,13 @@
-
-
 export const ServiceJson = {
-    "type": "service_account",
-    "project_id": "managment-refill",
-    "private_key_id": "af023f925ba0a97154eac3eaa2931b6903b28a75",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCgMiob+V6I6DBC\nGWbHcP57DSCaugDJ15JW1LmdcZ1BNGIWSjw69ap148vsi5sv342/glefqaGhGjzH\nkBJDuXHe77SmMDYw8upHEgxjZSzFzIL0vcDnQq/NcaXXjfWWD+318NT9aPrCQylp\n+EXewcOz09UlKtbeUJ1jI9GiPTssyJXd1WvltDX/vfsPTYoNMdMHOK1/Iit8usiV\nI7PT5NX6iXxfhabhifNjC4U3rLouQy3tPfM9IArdFjOA2R/olySHejlI2+UoTOmU\n1MxPBZWOPrZ6vG6KUWImaI/o3mUsL6Qcy6EQHOW2B/Y+uTeNiZGRHlEihkaXhaEG\n7Z7Y5tYLAgMBAAECggEAAMGRMVCcAZ7Bm45DI0Mv8E9qN/toxYse8F9Qq6LoUHEk\nMAwUUghBewz4NBr0leVC5gSCJV7HmdsQg7X1Csd1TM1qLN6iuC3CrVMzJm+ee9CF\n+GHCPKqJPzmqGWUHeQ3aMa77/QuMeMjts8etjL+dYjE9xaR4ydPXlQA2R8MXjxPh\nYX8fyg1CcddVOb4xLjt0UicGGxbZTfvc8KrYB0+WeMkhD0tVxogsve4HQ7Lqs0Xi\nDnZEX92RRs0yLMDaKwwgKQpR5CypklAN4PhSRM6/XSIHVB7jVUMroD7nd/xzdqGw\n1uxQNirgddIe7CaAl4sXzDO/uSI2bMrPA59sAkDgAQKBgQDXvN+dtazBmK6cV6lm\nHtRY0myaDTOybTOEDs/4bvytCH3TS9P1PuwYl7kJ+bgSqRT3B3WxRRZHpmmXifF7\njy+cnB38WTvUHUm0S7K4fn7YbVyRy3Fc5na9EM14RX7u8gVfzF+I+O5MuJ/NMeSq\nkeIydr8YSAoVbE3bQGXqI3k2iwKBgQC+F7byu0yPoSzU/9jkrvFu8gdyzLpI7Frq\ntNnG2jCdmhmU3UxlDj50o4Ml81KrOanNyNMm0x7i6H101YDPd4GQ4q/hfhBG21J/\nePesQnGhpCUSWrM+wchBJ1++EuM6S/fyJqP7jgFjcnF57dupFUs6K0yGd8eXgnkJ\nR9MLmRxOgQKBgQDAq/wB+5jYJaX8FwZBH9OBqBw50QSUm5ZgoGH4fBbJ6kWFibk4\n1ANDS22EjTNp5vvESzM/KHZncyXmotm6Af0BnyPglUkrfSSwEtMAlmhune0IgGs0\nDteIrxLyEQ72E8vc7Md9quQYPOLq/V49P6CGdSrhYqUu0NUPtQnP+/JrVQKBgQC1\nLScafHfBIU34iQ0xEKheJOICgaw/Gr6gmtRB2e2iGKCvKKjKXGQciUfTYPF27tOv\npevSQkBa5lnEhEVr96wO6m/VzHA5CIq9og71KLLi6+7FapsSgmGZ76wqHhhilMty\nCXAbr0IVhZswKgtFQV1cyK38L6eU+S7hMIqusDSxgQKBgQDK9LB49BbU6a1IDlNg\nul2+VhuPSf4sXx3hBDNKIPl3kFO7HKFDlLZzwvjcN4uGgoNklyAT/sC2VCOXcG6M\n7KTBkn20lb9QTYDRVDxb73RZefLngKuGfBVNmAHYn9+LRdhwLXciE0/qBDVhEKYt\nDcKwLV+YaZfP359ARzb8K/Wkig==\n-----END PRIVATE KEY-----\n",
-    "client_email": "aditya101@managment-refill.iam.gserviceaccount.com",
-    "client_id": "109743218614174611475",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/aditya101%40managment-refill.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
+    type: "service_account",
+    project_id: "managment-refill",
+    private_key_id: process.env.GCP_PRIVATE_KEY_ID,
+    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: "aditya101@managment-refill.iam.gserviceaccount.com",
+    client_id: "109743218614174611475",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/aditya101%40managment-refill.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com"
 }
